461e2a1f8a5b2e85c1cb1dd48e55e56868b8d3c5
Zac <a19860101@gmail.com>
2026-05-14 20:11:55 +0800

    tqc

--- PYD308.py
+++ PYD308.py
@@ -1,8 +1,10 @@
 # TODO
 
-
-
-
+n = int(input())
+for i in range(n):
+    s = input()
+    a = [int(item) for item in list(s)]
+    print(f'Sum of all digits of {s} is {sum(a)}')
 """
 Sum of all digits of _ is _
 """
