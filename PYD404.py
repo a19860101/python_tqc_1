cb68db52fa86104691ea17b8d5d121cc4de6ca2c
Zac <a19860101@gmail.com>
2026-05-14 20:30:23 +0800

    tqc

--- PYD404.py
+++ PYD404.py
@@ -1,1 +1,18 @@
 # TODO
+
+# 1
+s = int(input())
+s = str(s)
+s = reversed(s)
+print(''.join(s))
+
+# 2
+# s = input()
+# s = reversed(s)
+# print(''.join(s))
+
+# 3
+# print(''.join(reversed(input())))
+
+# 4
+# print(''.join(reversed(str(int(input())))))
