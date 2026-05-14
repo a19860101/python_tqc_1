bbb0f25cd11ed2a45139700ca1e1372bcabd5172
Zac <a19860101@gmail.com>
2026-05-14 21:32:10 +0800

    tqc

--- PYD710.py
+++ PYD710.py
@@ -1,9 +1,18 @@
 #TODO
 
     #Key: (後方有一空白格)
-    key = input("Key: ")
+    # key = input("Key: ")
     
     #Value: (後方有一空白格)
+    # value = input("Value: ")
+
+d = {}
+while True:
+    key = input("Key: ")
+    if key == 'end':
+        break
     value = input("Value: ")
+    d[key] = value
 
-
+search = input('Search key: ')
+print(search in d)
