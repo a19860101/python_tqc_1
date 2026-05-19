ce878f553a9417e643953448e6b5b846249efedc
Zac <a19860101@gmail.com>
2026-05-19 19:57:03 +0800

    tqc

--- PYD510.py
+++ PYD510.py
@@ -1,1 +1,12 @@
 # TODO
+def compute(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return compute(n-1) + compute(n-2)
+
+n = int(input())
+for i in range(n):
+    print(compute(i),end=' ')
