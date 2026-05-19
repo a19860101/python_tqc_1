3dc7e3ef4b1ea3cd598fa6f84acaeb93b34d687d
Zac <a19860101@gmail.com>
2026-05-19 19:46:05 +0800

    tqc

--- PYD906.py
+++ PYD906.py
@@ -3,7 +3,14 @@ str_old = input()
 str_new = input()
 #TODO
 print("=== Before the replacement")
+
+# data = open(f_name)
+# result = data.read()
+
+result = open(f_name).read()
+print(result)
 #TODO
 
 print("=== After the replacement")
 #TODO
+print(result.replace(str_old, str_new))
